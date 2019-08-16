da830ddb77b42facac77c35d617fba26ae1f44ef
Akramont <50740921+Akramont@users.noreply.github.com>
2019-08-16 20:04:47 +0800

    Добавление проекта

---
+++ Shuter/Assets/Script/FireBoll.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBoll : MonoBehaviour
+{
+    public Vector2 speed;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = speed;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Destroy")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Shuter/Assets/Script/PersonCharacter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonCharacter : MonoBehaviour
+{
+    public float delay;
+    private float shut = 0;
+    public float speed;
+    private Rigidbody2D rb2d;
+    private Animator anim;
+    public GameObject fireBall;
+    
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(move));
+        if (Input.GetKey(KeyCode.E) && Time.time > shut + delay)
+        {
+            anim.SetBool("Attack", true);
+            shut = Time.time;
+            Fire();
+        }
+        else
+        {
+            anim.SetBool("Attack", false);
+        }
+        Vector2 movement = new Vector2(move * speed, rb2d.velocity.y);
+        rb2d.velocity = movement;
+        AI.i++;
+    }
+
+    void Fire()
+    {
+        Instantiate(fireBall, transform.position + new Vector3(0.55f, -0.55f, 0), Quaternion.identity);
+    }
+}
